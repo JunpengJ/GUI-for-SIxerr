261dfb3d6f8f21178f7ce2ed8e8c0ef4f5749fc0
mckaylandon <mckaylandon@gmailcom>
2023-11-30 00:47:09 -0800

    Added lots of stuff to get freelancers to display...

---
+++ GUI for SIxerr/FreelanceUserControl.h
@@ -0,0 +1,105 @@
+#pragma once
+
+using namespace System;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+using namespace System::Windows::Forms;
+using namespace System::Data;
+using namespace System::Drawing;
+
+
+namespace GUIforSIxerr {
+
+	/// <summary>
+	/// Summary for FreelanceUserControl
+	/// </summary>
+	public ref class FreelanceUserControl : public System::Windows::Forms::UserControl
+	{
+	public:
+		FreelanceUserControl(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+		void SetData(/*String^ fieldName,*/ String^ fieldValue)
+		{
+			// Assuming you have labels in the tableLayoutPanel1
+			//Label^ labelFieldName = gcnew Label();
+			Label^ labelFieldValue = gcnew Label();
+
+			//labelFieldName->Text = fieldName;
+			labelFieldValue->Text = fieldValue;
+
+			// Add labels to the tableLayoutPanel1
+			//tableLayoutPanel1->Controls->Add(labelFieldName);
+			tableLayoutPanel1->Controls->Add(labelFieldValue);
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~FreelanceUserControl()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this->tableLayoutPanel1->CellBorderStyle = System::Windows::Forms::TableLayoutPanelCellBorderStyle::Single;
+			this->tableLayoutPanel1->ColumnCount = 3;
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
+				99)));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Absolute,
+				653)));
+			this->tableLayoutPanel1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12.0F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			this->tableLayoutPanel1->GrowStyle = System::Windows::Forms::TableLayoutPanelGrowStyle::FixedSize;
+			this->tableLayoutPanel1->Location = System::Drawing::Point(3, 3);
+			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
+			this->tableLayoutPanel1->RowCount = 1;
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::AutoSize, 100)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(1044, 82);
+			this->tableLayoutPanel1->TabIndex = 0;
+			this->tableLayoutPanel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &FreelanceUserControl::tableLayoutPanel1_Paint);
+			// 
+			// FreelanceUserControl
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->Controls->Add(this->tableLayoutPanel1);
+			this->Name = L"FreelanceUserControl";
+			this->Size = System::Drawing::Size(1050, 88);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void tableLayoutPanel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+	}
+	};
+}
--- GUI for SIxerr/Program.cpp
+++ GUI for SIxerr/Program.cpp
@@ -10,11 +10,91 @@
 #include "Setting.h"
 #include "SignUpForC.h"
 #include "SignUpForF.h"
+#include "FindFreelancers.h"
+#include "FreelanceUserControl.h"
+
+#include <string>
+#include <iostream>
+#include <vector>
 
 
 using namespace System;
 using namespace System::Windows::Forms;
+using namespace System::Data::OleDb;
 
+void MarshalString(String^ s, std::string& os) {
+	using namespace Runtime::InteropServices;
+	const char* chars =
+		(const char*)(Marshal::StringToHGlobalAnsi(s)).ToPointer();
+	os = chars;
+	Marshal::FreeHGlobal(IntPtr((void*)chars));
+}
+
+void PopulateFreelancers(std::vector <Freelancer> &freelancers) {
+	String^ connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\mckay\\Documents\\SixerrDB.accdb";
+	//String^ connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Marcus Hoang\\Documents\\SixerrDB.accdb";
+	String^ sqlQuery = "SELECT * FROM Users WHERE Desig = 'Freelancer'";
+
+	OleDbConnection^ connection = gcnew OleDbConnection(connString);
+
+	try
+	{
+		connection->Open();
+
+		OleDbCommand^ command = gcnew OleDbCommand(sqlQuery, connection);
+		OleDbDataReader^ reader = command->ExecuteReader();
+
+		while (reader->Read())
+		{
+			User^ freelancer = gcnew User();
+
+			String^ un = reader->GetString(1);
+			String^ p = reader->GetString(0);
+			String^ fn = reader->GetString(2);
+			String^ ln = reader->GetString(3);
+			int r = reader->GetInt32(4);
+			String^ d = reader->GetString(5);
+			
+			std::string unS, pS, fnS, lnS, dS;
+			MarshalString(un, unS);
+			MarshalString(p, pS);
+			MarshalString(fn, fnS);
+			MarshalString(ln, lnS);
+			MarshalString(d, dS);
+
+			Freelancer fl = Freelancer(unS, pS, fnS, lnS, r, dS);
+			freelancers.push_back(fl);
+		}
+	}
+	catch (Exception^ ex)
+	{
+		String^ errorMessage = String::Format("Error: {0}\nStackTrace: {1}", ex->Message, ex->StackTrace);
+		MessageBox::Show(errorMessage, "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	}
+	finally
+	{
+		if (connection != nullptr)
+		{
+			connection->Close();
+		}
+	}
+}
+
+void displayFreelancerVec(std::vector <Freelancer>& freelancers) {
+	String^ message = "";
+	for each (Freelancer freelancer in freelancers)
+	{
+		System::String^ un = gcnew System::String(freelancer.username.c_str());
+		System::String^ p = gcnew System::String(freelancer.pass.c_str());
+		System::String^ fn = gcnew System::String(freelancer.fName.c_str());
+		System::String^ ln = gcnew System::String(freelancer.lName.c_str());
+		System::String^ r = freelancer.rating.ToString();
+		System::String^ d = gcnew System::String(freelancer.desig.c_str());
+
+		message += String::Format("\nUsername: {0}, Password: {1}, FirstName: {2}, LastName: {3}, Rating: {4}, Desig: {5}\n", un, p, fn, ln, r, d);
+	}
+	MessageBox::Show(message, "Freelancer Data", MessageBoxButtons::OK, MessageBoxIcon::Information);
+}
 
 void main(array<String^>^ args) {
 	Application::EnableVisualStyles();
@@ -22,6 +102,9 @@ void main(array<String^>^ args) {
 	GUIforSIxerr::Interface form;
 	GUIforSIxerr::LOSC loginForm;
 	GUIforSIxerr::LOSF loginFormF;
+
+	PopulateFreelancers(freelancerVec);
+	//displayFreelancerVec(freelancerVec);
 
 	User^ user = nullptr;
 	while (true) {
@@ -71,5 +154,6 @@ void main(array<String^>^ args) {
 	else {
 		MessageBox::Show("Canceled", "Program.cpp", MessageBoxButtons::OK);
 	}
+
 
 }
